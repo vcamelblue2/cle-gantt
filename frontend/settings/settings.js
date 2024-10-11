64e70a9e52e9f2ecbbe988ed57594f041dce18a2
Vincenzo Villani <vincenzo.villani@studio.unibo.it>
2024-10-11 19:43:56 +0200

    fix graph

--- frontend/settings/settings.js
+++ frontend/settings/settings.js
@@ -1,4 +1,5 @@
 export const NUM_DAYS = 365*2
 export const DAY_SIZE_PX = 18
 export const HEDADER_WIDTH = 300
-export const GRAPH_WIDTH = ((NUM_DAYS*DAY_SIZE_PX)+(HEDADER_WIDTH)+20)
+export const ROW_HEIGHT = 30
+export const GRAPH_WIDTH = ((NUM_DAYS*DAY_SIZE_PX)+(HEDADER_WIDTH)+20)+(DAY_SIZE_PX*3) 
